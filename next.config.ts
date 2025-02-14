a8517b3d3dac2920a7ae28d3807a33a31f6fe31a
Max Jeschek <max@jeschek.dev>
2025-02-14 23:35:35 +0100

    added stockfish hopefully

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    outputFileTracingIncludes: {
+        "/api/stockfish": ["./stockfish.js"],
+    },
+};
 
 export default nextConfig;
