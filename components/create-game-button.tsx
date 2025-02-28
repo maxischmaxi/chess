1a359d18806bb49655427d60c8580ed7020c921f
Max Jeschek <max@jeschek.dev>
2025-02-28 16:38:31 +0100

    update

--- components/create-game-button.tsx
+++ components/create-game-button.tsx
@@ -17,72 +17,31 @@ import Image from "next/image";
 import { LocalGame } from "@/lib/definitions";
 
 export function CreateGameButton() {
-    const [errorMessage, setErrorMessage] = useState("");
     const [open, setOpen] = useState(false);
-    const [againstAi, setAgainstAi] = useState(true);
     const [preferredColor, setPreferredColor] = useState<"w" | "b" | "r">("w");
     const router = useRouter();
 
     async function createGame() {
-        if (againstAi) {
-            const id = v4();
-            const localGames = JSON.parse(
-                localStorage.getItem("games") || "[]",
-            );
-            let color: "w" | "b";
-            if (preferredColor === "r") {
-                color = Math.random() > 0.5 ? "w" : "b";
-            } else {
-                color = preferredColor;
-            }
-
-            const game: LocalGame = {
-                id,
-                fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-                player1: localStorage.getItem("id") ?? "",
-                player2: "ai",
-                color,
-                moves: [],
-            };
-            localGames.push(game);
-            localStorage.setItem("games", JSON.stringify(localGames));
-            router.push(`/game/bot/${id}`);
-            return;
+        const id = v4();
+        const localGames = JSON.parse(localStorage.getItem("games") || "[]");
+        let color: "w" | "b";
+        if (preferredColor === "r") {
+            color = Math.random() > 0.5 ? "w" : "b";
+        } else {
+            color = preferredColor;
         }
 
-        const id = localStorage.getItem("id");
-
-        if (!id) {
-            setErrorMessage(
-                "Sie müssen angemeldet sein, um ein Spiel zu erstellen",
-            );
-            return;
-        }
-
-        const res = await fetch("/api/game", {
-            method: "POST",
-            body: JSON.stringify({
-                player1: id,
-                player2: againstAi ? "ai" : "",
-                preferredColor,
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-
-        if (!res.ok) {
-            setErrorMessage("Spiel konnte nicht erstellt werden");
-            return;
-        }
-
-        if (res.status !== 200) {
-            setErrorMessage("Spiel konnte nicht erstellt werden");
-            return;
-        }
-
-        const { gameId } = await res.json();
-        router.push(`/game/${gameId}`);
+        const game: LocalGame = {
+            id,
+            fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            player1: localStorage.getItem("id") ?? "",
+            player2: "ai",
+            color,
+            moves: [],
+        };
+        localGames.push(game);
+        localStorage.setItem("games", JSON.stringify(localGames));
+        router.push(`/game/bot/${id}`);
     }
 
     return (
@@ -94,7 +53,7 @@ export function CreateGameButton() {
                 <DialogHeader>
                     <DialogTitle>Neues Spiel erstellen</DialogTitle>
                     <DialogDescription>
-                        {errorMessage || "Erstellen Sie ein neues Spiel"}
+                        {"Erstellen Sie ein neues Spiel"}
                     </DialogDescription>
                 </DialogHeader>
                 <div className="flex flex-row justify-center items-center gap-4">
@@ -136,20 +95,6 @@ export function CreateGameButton() {
                             height={50}
                             alt="Black King"
                         />
-                    </Button>
-                </div>
-                <div className="flex flex-row justify-center items-center gap-4">
-                    <Button
-                        onClick={() => setAgainstAi(true)}
-                        variant={againstAi ? "default" : "outline"}
-                    >
-                        AI
-                    </Button>
-                    <Button
-                        onClick={() => setAgainstAi(false)}
-                        variant={againstAi ? "outline" : "default"}
-                    >
-                        Spieler
                     </Button>
                 </div>
                 <DialogFooter>
